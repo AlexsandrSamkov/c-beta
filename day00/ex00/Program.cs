d1c9c12306c65c8299104ff575682f092e9a61f2
weambros <weambros@student.21-school.ru>
2021-05-23 23:41:39 +0300

    Test

---
+++ day00/ex00/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 5)
+            {
+                Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+                return;
+            }
+            if (args.Length > 5)
+            {
+                Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+                return;
+            }
+            double sum, rate, payment, resTerm, resPay;
+            int term, selectMount ;
+            Double.TryParse(args[0],out sum); 
+            Double.TryParse(args[1],out rate); 
+            Int32.TryParse(args[2], out term);
+            Int32.TryParse(args[3], out selectMount);
+            Double.TryParse(args[4], out payment);
+            if (sum <= 0 | rate <= 0 | payment <= 0 | term <= 0  | selectMount <= 0) 
+            {
+               Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+               return;
+            }
+            resPay = overpaymentPay(sum, rate, term, selectMount, payment);
+            resTerm = overpaymentTerm(sum, rate, term, selectMount, payment);
+            if (resPay == resTerm)
+            {
+                Console.WriteLine("Уменьшение платежа равно уменьшению срока");
+            }
+            else if (resPay > resTerm)
+            {
+                Console.WriteLine("Уменьшение срока выгоднее уменьшения платежа");
+            }
+            else if (resTerm > resPay)
+            {
+                Console.WriteLine("Уменьшение платежа выгоднее уменьшения срока");
+            }
+            Console.WriteLine("Переплата при уменьшении платежа: " + Math.Round(resPay,2) + "р.");
+            Console.WriteLine("Переплата при уменьшении срока: " + Math.Round(resTerm,2) + "р.");
+           if (resPay < resTerm)
+            {
+                Console.WriteLine("Уменьшение платежа выгоднее уменьшения срока на "
+                                  + Math.Round(resTerm - resPay,2) + "р.");
+            }
+            else if (resTerm < resPay)
+            {
+                Console.WriteLine("Уменьшение срока выгоднее уменьшения платежа на "
+                                  + Math.Round(resPay - resTerm ,2) + "р.");
+            }
+        }
+
+        static int ftDayYear()
+        {
+            DateTime thisDay = DateTime.Today;
+            return (DateTime.IsLeapYear(thisDay.Year) ? 366 : 365);
+        }
+        static double overpaymentPay(double sum, double rate, int term, int selectMount, double payment)
+        {
+            double loanCost, perMouthRate, per, perAll;
+            DateTime thisDay = DateTime.Today;
+            int yearDay, mouthDay, maxMouth;
+            perAll = 0;
+            maxMouth = term;
+            perMouthRate = rate / 12 / 100;
+            loanCost = (sum * perMouthRate * Math.Pow(1 + perMouthRate, term)) / (Math.Pow(1 + perMouthRate, term) - 1);
+            for (int j = 1; j <= maxMouth; j++)
+            {
+                
+                if (selectMount == j - 1)
+                {
+                    sum -= payment;
+                    term -= j - 1;
+                    loanCost = (sum * perMouthRate * Math.Pow(1 + perMouthRate, term))
+                               / (Math.Pow(1 + perMouthRate, term) - 1);
+                }
+                yearDay = ftDayYear();
+                mouthDay =  DateTime.DaysInMonth(thisDay.Year, thisDay.Month);
+                per = (sum * rate * mouthDay / 100.0 / yearDay);
+                thisDay = thisDay.AddMonths(1);
+                sum -= loanCost - per;
+                perAll += per;
+            }
+            return perAll;
+        }
+        static double overpaymentTerm(double sum, double rate, int term, int selectMount, double payment)
+        {
+            double loanCost, perMouthRate, per, perAll;
+            DateTime thisDay = DateTime.Today;
+            int yearDay, mouthDay, maxMouth;
+            perAll = 0;
+            maxMouth = term;
+            perMouthRate = rate / 12 / 100;
+            loanCost = (sum * perMouthRate * Math.Pow(1 + perMouthRate, term)) / (Math.Pow(1 + perMouthRate, term) - 1);
+            for (int j = 1; j <= maxMouth; j++)
+            {
+                
+                if (selectMount == j - 1)
+                {
+                    sum -= payment;
+                }
+                yearDay = ftDayYear();
+                mouthDay =  DateTime.DaysInMonth(thisDay.Year, thisDay.Month);
+                per = (sum * rate * mouthDay / 100.0 / yearDay);
+                thisDay = thisDay.AddMonths(1);
+                if (sum - loanCost <= 0)
+                {
+                    return (perAll += per);
+                }
+                sum -= loanCost - per;
+                perAll += per;
+            }
+            return perAll;
+        }
+    }
+}
---
+++ day00/ex01/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+namespace ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = string.Empty;
+            var line = string.Empty;
+            char key;
+            while (true)
+            {
+                Console.WriteLine(">Enter name:");
+                name = Console.ReadLine();
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Your name was not found");
+                    continue;
+                }
+                key = 'n';
+                using (var sr = new StreamReader("/Users/aleksandrsamkov/RiderProjects/ex01/ex01/names.txt", System.Text.Encoding.Default))
+                {
+                    
+                    while (!string.IsNullOrEmpty(line = sr.ReadLine()))
+                    {
+                        if (LevenshteinDistance(name, line) < 3)
+                        {
+                            Console.WriteLine("Did you mean “" + line + "”? Y/N");
+                            while (true)
+                            {
+                                key = Console.ReadKey().KeyChar;
+                                if (key == 'y' | key == 'Y' | key == 'n' | key == 'N')
+                                    break;
+                            }
+                            if (key == 'y' | key == 'Y')
+                                break;
+                        }
+                    }
+                }
+                if (key == 'n' | key == 'N')
+                    Console.WriteLine("Your name was not found.");
+                Console.ReadKey();
+            }
+        }
+       static int LevenshteinDistance(string string1, string string2)
+        {
+            if (string1 == null) throw new ArgumentNullException("string1");
+            if (string2 == null) throw new ArgumentNullException("string2");
+            int diff;
+            int[,] m = new int[string1.Length + 1, string2.Length + 1];
+
+            for (int i = 0; i <= string1.Length; i++) { m[i, 0] = i; }
+            for (int j = 0; j <= string2.Length; j++) { m[0, j] = j; }
+
+            for (int i = 1; i <= string1.Length; i++)
+            {
+                for (int j = 1; j <= string2.Length; j++)
+                {
+                    diff = (string1[i - 1] == string2[j - 1]) ? 0 : 1;
+
+                    m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1, m[i, j - 1] + 1), m[i - 1, j - 1] + diff);
+                }
+            }
+            return m[string1.Length, string2.Length];
+        }
+    }
+}
